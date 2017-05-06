d3d5d496f4ff50a99f63c27288e41b1e5d356b4c
animesh <animesh@ebctechnologies.com>
2017-05-06 23:50:09 +0530

    adding source and sink actor
    Signed-off-by: animesh <animesh@ebctechnologies.com>

--- src/main/scala/MyFirstStream.scala
+++ src/main/scala/MyFirstStream.scala
@@ -1,6 +1,7 @@
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
+import scala.concurrent.duration._
 
 /**
   *
@@ -22,6 +23,14 @@ object MyFirstStream {
     implicit val materializer = ActorMaterializer()
 
     // stream definition and execution code goes here
+    // A perfectly useless stream
+    val stream1 = Source.maybe.to(Sink.ignore)
+    //stream1.run()
+
+    // A stream of a repeating element to standard output using tick
+    val stream4 = Source.tick(0 seconds, 2 seconds, "Hello World!").to(Sink.foreach(println))
+    stream4.run()
+
   }
 
 }
