de05ac756901579a0f3efbaa1d5143b3a1933e27
animesh <animesh@ebctechnologies.com>
2017-07-03 23:35:40 +0530

    kafka sink source example
    Signed-off-by: animesh <animesh@ebctechnologies.com>

--- build.sbt
+++ build.sbt
@@ -7,3 +7,11 @@ scalaVersion := "2.12.2"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-stream-kafka" % "0.16"
 libraryDependencies +=  "com.typesafe.akka" %% "akka-stream" % "2.4.17"
+libraryDependencies ++= Seq(
+"com.typesafe.akka" %% "akka-http" % "10.0.1",
+"net.cakesolutions" %% "scala-kafka-client-testkit" % "0.10.2.0",
+"ch.qos.logback" % "logback-classic" % "1.1.3",
+"org.slf4j" % "log4j-over-slf4j" % "1.7.12",
+"com.typesafe.scala-logging" %% "scala-logging" % "3.5.0",
+"com.typesafe.akka" %% "akka-stream-kafka" % "0.14"
+)
---
+++ src/main/scala/Kafka.scala
@@ -0,0 +1,98 @@
+/**
+  * Created by animesh on 7/3/17.
+  */
+import akka.actor.ActorSystem
+import akka.kafka.{ ConsumerSettings, ProducerMessage, ProducerSettings, Subscriptions }
+import akka.kafka.scaladsl.{ Consumer, Producer }
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{ Sink, Source }
+import cakesolutions.kafka.testkit.KafkaServer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.{ StringDeserializer, StringSerializer }
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+
+object Kafka extends App {
+  // Needed for streams to leverage actors for their functionality
+  implicit val system = ActorSystem("MyActorSystemForKafka")
+  implicit val materializer = ActorMaterializer()
+
+  val kafkaPort = 9092
+  val kafkaLocation = s"localhost:$kafkaPort"
+  val kafkaGroupId = "myGroup"
+  val kafkaTopic = "myTopic"
+
+  new KafkaServer(kafkaPort = kafkaPort).startup()
+
+  val consumerSettings = ConsumerSettings(system, new StringDeserializer, new StringDeserializer)
+    .withBootstrapServers(kafkaLocation)
+    .withGroupId(kafkaGroupId)
+  val producerSettings = ProducerSettings(system, new StringSerializer, new StringSerializer)
+    .withBootstrapServers(kafkaLocation)
+
+  val kafkaSource = {
+    Consumer.plainSource(consumerSettings, Subscriptions.topics(kafkaTopic))
+    //    Source.fromPublisher(
+    //      Consumer
+    //        .plainSource(consumerSettings, Subscriptions.topics(kafkaTopic))
+    //        .runWith(Sink.asPublisher(fanout = true))
+    //    )
+  }
+  val kafkaSink = Producer.plainSink(producerSettings)
+
+
+
+  // A stream which periodically produces messages to Kafka
+  val stream1 = Source
+    .tick(0 seconds, 2 seconds, "Hello Kafka!")
+    .map(value => new ProducerRecord[String, String](kafkaTopic, value))
+    .to(kafkaSink)
+  //stream1.run()
+
+
+
+  // A stream which listens to messages from Kafka and sends them to the standard out
+  val stream2 = kafkaSource.to(Sink.foreach(record => println(record.value())))
+  //stream2.run()
+
+
+
+  // A stream which listens to messages from Kafka, modifies them then sends them back to Kafka
+  val toAdd = "!!1!"
+  val stream3 = kafkaSource
+    .map(_.value())
+    .filterNot(_.contains(toAdd))
+    .map(_ + toAdd)
+    .map(value => new ProducerRecord[String, String](kafkaTopic, value))
+    .to(kafkaSink)
+  //stream3.run()
+
+
+
+  val committableKafkaSource = Consumer.committableSource(consumerSettings, Subscriptions.topics(kafkaTopic))
+  val committableKafkaSink = Producer.commitableSink(producerSettings)
+
+  // A stream which commits the offset back to Kafka with a async map function
+  val stream4 = committableKafkaSource
+    .mapAsync(1)(msg => msg.committableOffset.commitScaladsl().map(_ => msg.record))
+    .map(_.value())
+    .map(msg => new ProducerRecord[String, String](kafkaTopic, msg))
+    .to(kafkaSink)
+  //stream4.run()
+
+
+
+  // A stream which uses a ccommittable Sink to commit the offset back to Kafka
+  val stream5 = committableKafkaSource
+    .map { msg =>
+      ProducerMessage.Message(
+        new ProducerRecord[String, String](kafkaTopic, msg.record.value()),
+        msg.committableOffset
+      )
+    }
+    .to(committableKafkaSink)
+  //stream5.run()
+}
